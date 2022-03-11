db16853e6517fe01c5ab2f9e70d80eccc9461da4
Juwon-Yun <eooll2@daum.net>
2022-03-11 20:20:28 +0900

    feat: shose before iter

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,58 @@
 import './App.css';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { useState } from 'react';
+import Data from './data.js'
+
 
 function App() {
+
+  let [shose, setShose] = useState(Data);
+
   return (
     <div className="App">
-      
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="container">
+        <div className="column">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="" />
+            <h4> { shose[0].title } </h4>
+            <p> { shose[0].content }</p>
+          </div>
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" width="100%" />
+            <h4> { shose[1].title }</h4>
+            <p> { shose[1].content}</p>
+          </div>
+          <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" width="100%" />
+            <h4>{shose[2].title}</h4>
+            <p> {shose[2].content} </p>
+          </div>
+        </div>
+      </div>
+
     </div>
+
+
   );
 }
 
