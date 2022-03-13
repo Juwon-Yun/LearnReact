76f57eef68eeab0aff73834dfccccfbdc45e307c
Juwon-Yun <eooll2@daum.net>
2022-03-13 13:49:41 +0900

    Modify: useEffect

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -33,15 +33,17 @@ function Detail(props) {
   // find() method => 주어진 판별 함수를 만족하는 첫 번째 요소의 값을 반환한다. 없으면 undefined 반환
   let findContent = props.data.find(content => content.id === +id)
 
-  let [alertFlage, setAlertFlag] = useState(true)
+  let [alertFlag, setAlertFlag] = useState(true)
 
   // useEffect hook은 적은 순서대로 사용
   // useEffect hook 3 여러가지 useEffect를 사용할 때 여러개 쓰면됨
   // useEffect hook 1, 컴포넌트가 렌더링될 때, mount
   useEffect(() => { 
-    setTimeout(() => {
-      setAlertFlag(false)
-    }, 2000);
+    // setTimeout(() => {
+    //   setAlertFlag(false)
+    // }, 2000);
+    // // input데이터가 바뀔때마다 계속나옴
+    // console.log('updating')
   })
 
   // useEffect hook 2, 컴포넌트가 사라질 때, unmount
@@ -51,7 +53,22 @@ function Detail(props) {
     }
   })
 
+  // useEffect hook 4, useEffect가 실행 될 조건 ex) alerFlag의 state가 바뀔때만!
+  // 조건이 빈간이면 [] => 해당 컴포넌트 ( 지금은 Detail )가 Update할 때도 실행안됨
+  // 즉 페이지 로딩때 한번만 일어남
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlertFlag(false)
+    }, 2000);
+    console.log('updating')
 
+    // settimeout 제거
+    return () => { 
+      clearTimeout(timer)
+    }
+   }, [alertFlag])
+
+  let [input, setInput] = useState('')
 
   return (
     <div className="container">
@@ -61,8 +78,23 @@ function Detail(props) {
         {/* <Title color='white'> Detail </Title> */}
         <Title className='red'> Detail </Title>
       </Box>
-      <div style={{'display' : alertFlage ? 'block': 'none'}} className="my-alert">
-        <p>재고가 얼마남지 않았습니다.</p>
+      {/* 
+        <div style={{'display' : alertFlage ? 'block': 'none'}} className="my-alert">
+          <p>재고가 얼마남지 않았습니다.</p>
+        </div> 
+      */}
+      
+      { /* 좋은 관습적 답 */
+        alertFlag ? 
+          (<div className="my-alert">
+            <p>재고가 얼마남지 않았습니다.</p>
+          </div>)
+        : null
+      }
+      {/* input data가 바뀌면 detail이 계속 재렌더링, 즉 계속 update된다. */}
+      <input onChange={(e) => { setInput(e.target.value )}}/>
+      <div>
+        {input}
       </div>
       <div className="row">
       <div className="col-md-6">
